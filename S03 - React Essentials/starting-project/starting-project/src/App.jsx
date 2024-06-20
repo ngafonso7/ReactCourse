53d0c88d06300511fdd42cf1b1a140024f89d003
Natanael Afonso <nagonso7@gmail.com>
2024-06-20 20:23:52 -0300

    S03 - ReactEssensials
    Adding starting-project files
    
    Signed-off-by: Natanael Afonso <nagonso7@gmail.com>

---
+++ S03 - React Essentials/starting-project/starting-project/src/App.jsx
@@ -0,0 +1,40 @@
+
+import { CORE_CONCEPTS } from './data.js';
+
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept 
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}/>
+          </ul>
+        </section> 
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ S03 - React Essentials/starting-project/starting-project/src/components/TabButton.jsx
@@ -0,0 +1,6 @@
+
+const TabButton = (props) => {
+    return <li><button>{props.children}</button></li>
+};
+
+export default TabButton;
