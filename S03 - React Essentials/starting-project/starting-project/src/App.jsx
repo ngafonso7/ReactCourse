8069106666ac7e3a060ac4df2aa24fa991c62958
Natanael Afonso <ngafonso7@gmail.com>
2024-06-24 13:50:06 -0300

    S03 - show content based on a useState

--- S03 - React Essentials/starting-project/starting-project/src/App.jsx
+++ S03 - React Essentials/starting-project/starting-project/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
-import { CORE_CONCEPTS } from "./data.js";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 const App = () => {
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
   const handleSelect = (selectedButton) => {
     setSelectedTopic(selectedButton);
@@ -41,7 +41,13 @@ const App = () => {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
