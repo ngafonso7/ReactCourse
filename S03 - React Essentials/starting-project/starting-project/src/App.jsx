e78e5da740c0daeca0f728583be893074500213f
Natanael Afonso <ngafonso7@gmail.com>
2024-06-24 16:19:31 -0300

    S04 - Using Fragment

--- S03 - React Essentials/starting-project/starting-project/src/App.jsx
+++ S03 - React Essentials/starting-project/starting-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 
@@ -15,7 +15,7 @@ const App = () => {
   };
 
   return (
-    <div>
+    <Fragment>
       <Header />
       <main>
         <section id="core-concepts">
@@ -66,7 +66,60 @@ const App = () => {
           )}
         </section>
       </main>
-    </div>
+    </Fragment>
+    // OR
+    // <>
+    //   <Header />
+    //   <main>
+    //     <section id="core-concepts">
+    //       <h2>Core Concepts</h2>
+    //       <ul>
+    //         {CORE_CONCEPTS.map((conceptItem) => (
+    //           <CoreConcept key={conceptItem.title} {...conceptItem} />
+    //         ))}
+    //       </ul>
+    //     </section>
+    //     <section id="examples">
+    //       <h2>Examples</h2>
+    //       <menu>
+    //         <TabButton
+    //           isSelected={selectedTopic == "components"}
+    //           onSelect={() => handleSelect("components")}
+    //         >
+    //           Components
+    //         </TabButton>
+    //         <TabButton
+    //           isSelected={selectedTopic == "jsx"}
+    //           onSelect={() => handleSelect("jsx")}
+    //         >
+    //           JSX
+    //         </TabButton>
+    //         <TabButton
+    //           isSelected={selectedTopic == "props"}
+    //           onSelect={() => handleSelect("props")}
+    //         >
+    //           Props
+    //         </TabButton>
+    //         <TabButton
+    //           isSelected={selectedTopic == "state"}
+    //           onSelect={() => handleSelect("state")}
+    //         >
+    //           State
+    //         </TabButton>
+    //       </menu>
+    //       {!selectedTopic && <p>Please select a topic.</p>}
+    //       {selectedTopic && (
+    //         <div id="tab-content">
+    //           <h3>{EXAMPLES[selectedTopic].title}</h3>
+    //           <p>{EXAMPLES[selectedTopic].description}</p>
+    //           <pre>
+    //             <code>{EXAMPLES[selectedTopic].code}</code>
+    //           </pre>
+    //         </div>
+    //       )}
+    //     </section>
+    //   </main>
+    // </>
   );
 };
 
