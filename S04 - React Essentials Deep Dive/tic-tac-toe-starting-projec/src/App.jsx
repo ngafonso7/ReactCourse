c0b33f1ddd8257241017fc5e6458098ca0c402e5
Natanael Afonso <ngafonso7@gmail.com>
2024-06-25 13:54:53 -0300

    S04 - Tic Tac Toe uper useState

--- S04 - React Essentials Deep Dive/tic-tac-toe-starting-projec/src/App.jsx
+++ S04 - React Essentials Deep Dive/tic-tac-toe-starting-projec/src/App.jsx
@@ -1,19 +1,40 @@
+import { useState } from "react";
+
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
-    return (
-        <main>
-            <div id="game-container">
-                <ol id="players">
-                    <Player initialName="Player 1" symbol="X" />
-                    <Player initialName="Player 2" symbol="O" />
-                </ol>
-                <GameBoard />
-            </div>
-            LOGS
-        </main>
-    );
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  const handleSelectSquare = () => {
+    setActivePlayer((currentActivePlayer) => {
+      return currentActivePlayer === "X" ? "O" : "X";
+    });
+  };
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
+        </ol>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
+      </div>
+      LOGS
+    </main>
+  );
 }
 
 export default App;
--- S04 - React Essentials Deep Dive/tic-tac-toe-starting-projec/src/components/Player.jsx
+++ S04 - React Essentials Deep Dive/tic-tac-toe-starting-projec/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Player = ({ initialName, symbol }) => {
+const Player = ({ initialName, symbol, isActive }) => {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -28,7 +28,7 @@ const Player = ({ initialName, symbol }) => {
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
