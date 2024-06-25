38f978cf0275853d70495cb6b6da18ee086d0f84
Natanael Afonso <nagonso7@gmail.com>
2024-06-25 20:04:12 -0300

    S04 - Implement logic to check winner and create simple gameover

--- S04 - React Essentials Deep Dive/tic-tac-toe-starting-projec/src/App.jsx
+++ S04 - React Essentials Deep Dive/tic-tac-toe-starting-projec/src/App.jsx
@@ -4,6 +4,15 @@ import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 
+import { WINNING_COMBINATIONS } from './winning-combinations.js'
+import GameOver from "./components/GameOver.jsx";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function deriveActivePlayer(gameTurns) {
     let currentPlayer = "X";
 
@@ -19,6 +28,32 @@ function App() {
   //const [activePlayer, setActivePlayer] = useState("X");
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol && 
+      firstSquareSymbol === secondSquareSymbol && 
+      firstSquareSymbol === thirdSquareSymbol) {
+        winner = firstSquareSymbol;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   const handleSelectSquare = (rowIndex, colIndex) => {
     // setActivePlayer((currentActivePlayer) => {
@@ -57,7 +92,8 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {(winner || hasDraw) && <GameOver winner={winner}/>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns}/>
     </main>
--- S04 - React Essentials Deep Dive/tic-tac-toe-starting-projec/src/components/GameBoard.jsx
+++ S04 - React Essentials Deep Dive/tic-tac-toe-starting-projec/src/components/GameBoard.jsx
@@ -1,20 +1,9 @@
 import { useState } from "react";
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
 
-const GameBoard = ({ onSelectSquare, turns }) => {
-  let gameBoard = initialGameBoard;
 
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
+const GameBoard = ({ onSelectSquare, board }) => {
+  
 
   //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
@@ -30,12 +19,15 @@ const GameBoard = ({ onSelectSquare, turns }) => {
 
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
