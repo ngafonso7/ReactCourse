e1f3136866a5f3c883a8c33fec93d90e8fe473ba
Natanael Afonso <ngafonso7@gmail.com>
2024-06-25 13:42:15 -0300

    S04 - Initial GameBoard logic

--- S04 - React Essentials Deep Dive/tic-tac-toe-starting-projec/src/components/Player.jsx
+++ S04 - React Essentials Deep Dive/tic-tac-toe-starting-projec/src/components/Player.jsx
@@ -1,41 +1,41 @@
 import { useState } from "react";
 
 const Player = ({ initialName, symbol }) => {
-    const [playerName, setPlayerName] = useState(initialName);
-    const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
-    const handleClick = () => {
-        setIsEditing((editing) => !editing);
-    };
+  const handleClick = () => {
+    setIsEditing((editing) => !editing);
+  };
 
-    const handleChange = (event) => {
-        setPlayerName(event.target.value);
-    };
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  };
 
-    let editablePlayerName = <span className="player-name">{playerName}</span>;
-    let btnCaption = "Edit";
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
 
-    if (isEditing) {
-        editablePlayerName = (
-            <input
-                type="text"
-                required
-                value={playerName}
-                onChange={handleChange}
-            ></input>
-        );
-        btnCaption = "Save";
-    }
-
-    return (
-        <li>
-            <span className="player">
-                {editablePlayerName}
-                <span className="player-symbol">{symbol}</span>
-            </span>
-            <button onClick={handleClick}>{btnCaption}</button>
-        </li>
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChange}
+      ></input>
     );
+    btnCaption = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClick}>{btnCaption}</button>
+    </li>
+  );
 };
 
 export default Player;
