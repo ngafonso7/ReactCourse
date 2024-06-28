4aa8ed9841994c88122dc5d2c12cdf384eb31832
Natanael Afonso <ngafonso7@gmail.com>
2024-06-28 17:01:16 -0300

    S08 - Create Result dialog with modal

--- S08 - Refs and Portals/refs-and-portals-project/src/components/TimerChanllenge.jsx
+++ S08 - Refs and Portals/refs-and-portals-project/src/components/TimerChanllenge.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { useState } from "react";
+import ResultModal from "./ResultModal";
 
 const TimerChallenge = ({ title, targetTime }) => {
     const [timerExpired, setTimerExpired] = useState(false);
@@ -21,21 +22,25 @@ const TimerChallenge = ({ title, targetTime }) => {
     }
 
     return (
-        <section className="challenge">
-            <h2>{title}</h2>
-            {timerExpired && <p>You lost!</p>}
-            <p className="challenge-time">
-                {targetTime} second{targetTime > 1 ? "s" : ""}
-            </p>
-            <p>
-                <button onClick={timerStarted ? handleStop : handleStart}>
-                    {timerStarted ? "Stop" : "Start"} Challenge
-                </button>
-            </p>
-            <p className={timerStarted ? "active" : undefined}>
-                {timerStarted ? "Time is running" : "Time inactive"}
-            </p>
-        </section>
+        <>
+            {timerExpired && (
+                <ResultModal targetTime={targetTime} result={"lost"} />
+            )}
+            <section className="challenge">
+                <h2>{title}</h2>
+                <p className="challenge-time">
+                    {targetTime} second{targetTime > 1 ? "s" : ""}
+                </p>
+                <p>
+                    <button onClick={timerStarted ? handleStop : handleStart}>
+                        {timerStarted ? "Stop" : "Start"} Challenge
+                    </button>
+                </p>
+                <p className={timerStarted ? "active" : undefined}>
+                    {timerStarted ? "Time is running" : "Time inactive"}
+                </p>
+            </section>
+        </>
     );
 };
 
