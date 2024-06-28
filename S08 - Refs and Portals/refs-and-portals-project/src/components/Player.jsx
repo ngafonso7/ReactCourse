3651624e8e511e1e164a1714bba7b8f0971ddba5
Natanael Afonso <ngafonso7@gmail.com>
2024-06-28 15:51:10 -0300

    S08 - Start using ref to manipulate values

--- S08 - Refs and Portals/refs-and-portals-project/src/components/Player.jsx
+++ S08 - Refs and Portals/refs-and-portals-project/src/components/Player.jsx
@@ -1,24 +1,20 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
-  const [playerName, setPlayerName] = useState("");
-  const [nameDefined, setNameDefined] = useState(false);
+  const playerNameRef = useRef();
 
-  const handleChange = (event) => {
-    setNameDefined(false);
-    setPlayerName(event.target.value);
-  };
+  const [playerName, setPlayerName] = useState("");
 
   const handleClick = () => {
-    setNameDefined(true);
+    setPlayerName(playerNameRef.current.value);
   };
 
   return (
     <section id="player">
-      {/* <h2>Welcome {`${nameDefined ? playerName : "unknown entity"}`}</h2> */}
-      <h2>Welcome {nameDefined ? playerName : "unknown entity"}</h2>
+      {/* <h2>Welcome {`${playerName ? playerName : "unknown entity"}`}</h2> */}
+      <h2>Welcome {playerName ?? "unknown entity"}</h2>
       <p>
-        <input onChange={handleChange} value={playerName} type="text" />
+        <input ref={playerNameRef} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
